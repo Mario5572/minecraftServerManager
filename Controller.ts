3060c6b6ff0517ab84605d532cf856a589cbffff
Mario Gallego <mariogallegon@gmail.com>
2024-12-09 12:22:32 +0100

    Added the main Controller of the app

---
+++ Controller.ts
@@ -0,0 +1,38 @@
+import { ShellController } from "./ShellController";
+import { MinecraftServer } from "./minecraftServer";
+export class Controller{
+    private servers : MinecraftServer[];
+    private shell : ShellController;
+    
+    constructor(shell : ShellController | undefined = undefined){
+        if(shell == undefined) this.shell = new ShellController(ShellController.serverStates.NoServer)
+        else this.shell = shell;
+        this.servers = [];
+    }
+
+    redirectServerOutputToCommandLine() : void{
+        this.shell.redirectServerOutputToCommandLine()
+    }
+
+    addServer(server : MinecraftServer) : void{
+        this.servers.push(server);
+    }
+    getServers() : MinecraftServer[]{
+        return this.servers;
+    }
+    getCurrentState() : string{
+        return this.shell.getCurrentState()
+    }
+    async bootUpServer(name: string): Promise<void> {
+        for (const server of this.servers) {
+            console.log(server.getName(), name)
+            if (server.getName() == name) {
+                console.log("He entrado aqui")
+                await this.shell.bootUp(server); // Espera a que termine la operación
+                return; // Finaliza la función si se encuentra el servidor
+            }
+        }
+        throw new Error("Couldn't find a server with that name");
+    }
+
+}
