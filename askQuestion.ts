f872b0a2ca3b15dc621ad8483abb95263c4555a2
Mario Gallego <mariogallegon@gmail.com>
2024-12-09 12:20:42 +0100

    Added module to request user input

---
+++ askQuestion.ts
@@ -0,0 +1,21 @@
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: ''
+});
+
+// Helper para hacer preguntas al usuario
+export const askQuestion = (query: string): Promise<string> => {
+    return new Promise((resolve) => {
+        process.stdout.write(query);
+        rl.question(query, (answer) => {
+            resolve(answer.trim());
+        });
+    });
+};
+
+export const closeReadLine = () : void => {
+    rl.close();
+}
