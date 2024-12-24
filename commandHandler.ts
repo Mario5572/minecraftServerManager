fa08e4b01b0dfa97052d8d8131610c66155a231d
Mario Gallego Navarro <mariogallegon@gmail.com>
2024-12-24 16:52:17 +0100

    Added stop,redirect,stopRedirect command

--- commandHandler.ts
+++ commandHandler.ts
@@ -23,7 +23,11 @@ async function handleCommand(input : string) : Promise<void>{
         console.log("WELCOME TO THE SERVER MANAGER\n")
         console.log("The thing that appears between [] is the current state of the Controller, wether its running a server, loading it, etc\n")
         console.log("   -create : asks you information to create a minecraft server, you can drag a file to the terminal to paste its route\n")
-    }
+        console.log("   -run : runs a server, run it by the name of the server you want to run\n")
+        console.log("   -stop : stops a server, stop it by the name of the server you want to stop\n")
+        console.log("   -redirect : redirects the output of the server to the command line\n")
+        console.log("   -stopredirect : stops the server and redirects the output to the command line\n")
+    }   
     else if(command === 'run'){
         if(!args[1]){
             console.log("Either youre a funny guy or illiterate, just type help")
@@ -37,6 +41,24 @@ async function handleCommand(input : string) : Promise<void>{
     }
     else if(command === 'load'){
         
+    }
+    else if(command === 'stop'){
+        if(!args[1]){
+            console.log("Either youre a funny guy or illiterate, just type help")
+            return;
+        }
+        try {
+            await controller.stopServer(args[1])
+        } catch (error) {
+            console.log("Coulnd stop the server, due to: ")
+        }
+    }
+    else if(command == 'redirect'){
+        controller.redirectServerOutputToCommandLine()
+    }
+    else if(command == 'stopredirect'){
+        controller.stopServer(args[1])
+        controller.redirectServerOutputToCommandLine()
     }
     else{
         console.log("You seem a little lost, need any \x1b[1mhelp\x1b[0m (there is a command help in case you are wondering)")
